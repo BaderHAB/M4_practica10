dbcbe9a8b0b11bfebf9079c097401156a685c60f
karen <klisse22@jaumebalmes.net>
2023-01-23 20:14:05 +0100

    kf

---
+++ exercici2_B.py
@@ -0,0 +1,13 @@
+#Creacion de una tupla
+myTupla = (1,2,3,4,5,6)
+
+#longitud de la tupla
+print(len(myTupla))
+
+#si un valor esta incluido en la tupla
+x = 2
+myTupla = (1,2,3,4,3,6)
+print (x in myTupla)
+
+#cuantas veces se repite un valor
+print(myTupla.count(3))
